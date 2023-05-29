714eb4cb6025df62ee927e27d7c30a56387d883b
Emesson-cmd <emerssosnkavallcante@gmail.com>
2023-05-28 22:16:01 -0300

    defining layout

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,21 +1,13 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import NavBar from '../../components/NavBar';
 import Header from './components/Header';
 import Form from './components/Form';
 
 export default function Reserve() {
   return (
-    <main>
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <div className="border-top h-screen">
-          <div className="py-9 w-3/5 m-auto">
-            <Header />
-            <Form />
-          </div>
-        </div>
-      </main>
-    </main>
+    <div className="border-top h-screen">
+      <div className="py-9 w-3/5 m-auto">
+        <Header />
+        <Form />
+      </div>
+    </div>
   );
 }
