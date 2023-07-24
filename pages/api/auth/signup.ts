9c1f396d28116e625a4bbe1c2a7d037f87681ef1
Emesson-cmd <emerssosnkavallcante@gmail.com>
2023-07-24 20:34:27 -0300

    generating jwt token

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import validator from 'validator';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt';
+import * as jose from 'jose';
 
 const prisma = new PrismaClient();
 
@@ -80,8 +81,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       },
     });
 
+    const alg = 'HS256';
+
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET)
+
+    const token = await new jose.SignJWT({
+      email: user.email,
+    })
+      .setProtectedHeader({ alg })
+      .setExpirationTime('24h')
+      .sign(secret)
+
     res.status(200).json({
-      hello: user,
+      hello: token,
     });
   }
 }
