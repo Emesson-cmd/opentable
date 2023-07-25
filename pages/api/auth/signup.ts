b8e8bace09d03204884de026067b4d7c0012c263
Emesson-cmd <emerssosnkavallcante@gmail.com>
2023-07-24 21:02:23 -0300

    sign in method

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -83,17 +83,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const alg = 'HS256';
 
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET)
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
     const token = await new jose.SignJWT({
       email: user.email,
     })
       .setProtectedHeader({ alg })
       .setExpirationTime('24h')
-      .sign(secret)
+      .sign(secret);
 
-    res.status(200).json({
-      hello: token,
-    });
+    return res.status(200).json({ token });
   }
+
+  return res.status(404).json('Unknown endpoint');
 }
