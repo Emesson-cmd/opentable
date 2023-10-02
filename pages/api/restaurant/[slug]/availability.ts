3fbc4f777444ecedfbdf652512884ce25251f7d8
Emesson-cmd <emerssosnkavallcante@gmail.com>
2023-10-02 11:31:33 -0300

    chore: adding some comments and availabilities options

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -4,6 +4,13 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 const prisma = new PrismaClient();
 
+/**
+ * This endpoint will be used to check the availability of a specific date and time
+ *
+ * @param req
+ * @param res
+ * @returns
+ */
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   const { slug, day, time, partySize } = req.query as {
     slug: string;
@@ -18,6 +25,7 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
     });
   }
 
+  // Gets the searchTimes of the time sent by user
   const searchTimes = timeAndSearch.find((t) => {
     return t.time === time;
   })?.searchTimes;
@@ -28,6 +36,7 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
     });
   }
 
+  // Gets all the bookings between the first and last searchTimes
   const bookings = await prisma.booking.findMany({
     where: {
       booking_time: {
@@ -42,8 +51,19 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
     },
   });
 
+  /**
+   "bookingTablesObg": {
+        "2023-09-17T12:00:00.000Z": {
+            "1": true
+        }
+    }
+   */
   const bookingTablesObg: { [key: string]: { [key: number]: true } } = {};
 
+  /**
+   * Gets all bookings from the date and time range (searchTime)
+   * Will transform "bookings" in an array of objects as above
+   */
   bookings.forEach((booking) => {
     bookingTablesObg[booking.booking_time.toISOString()] = booking.tables.reduce((obj, table) => {
       return {
@@ -53,6 +73,73 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
     }, {});
   });
 
-  return res.json({ searchTimes, bookings, bookingTablesObg });
+  // Return the tables from the restaurant which the user is trying to book
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      tables: true,
+      open_time: true,
+      close_time: true,
+    },
+  });
+
+  if (!restaurant) {
+    return res.status(400).json({
+      errorMessage: 'Invalid data provided',
+    });
+  }
+
+  const tables = restaurant.tables;
+
+  // Returns an array with times and table
+  const searchTimesWithTables = searchTimes.map((searchTime) => {
+    return {
+      date: new Date(`${day}T${searchTime}`),
+      time: searchTime,
+      tables,
+    };
+  });
+
+  // Remove time option from search time if it has already been taken
+  searchTimesWithTables.forEach((t) => {
+    t.tables = t.tables.filter((table) => {
+      if (bookingTablesObg[t.date.toDateString()]) {
+        if (bookingTablesObg[t.date.toDateString()][table.id]) return false;
+      }
+      return true;
+    });
+  });
+
+  const availabilities = searchTimesWithTables
+    .map((t) => {
+      const sumSeats = t.tables.reduce((sum, table) => {
+        return sum + table.seats;
+      }, 0);
+
+      return {
+        time: t.time,
+        available: sumSeats >= parseInt(partySize),
+      };
+    })
+    .filter((availability) => {
+      const timeIsAfterOpeningHour =
+        new Date(`${day}T${availability.time}`) >= new Date(`${day}T${restaurant.open_time}`);
+      const timeIsBeforeClosingHours =
+        new Date(`${day}T${availability.time}`) <= new Date(`${day}T${restaurant.close_time}`);
+
+      return timeIsAfterOpeningHour && timeIsBeforeClosingHours;
+    });
+
+  return res.json({
+    searchTimes,
+    bookings,
+    bookingTablesObg,
+    searchTimesWithTables,
+    availabilities,
+  });
 }
 
+// http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-01-01&time=23:00:00.000Z&partySize=4
+// vivaan-fine-indian-cuisine-ottawa
